2adeb760e2c25745e65fa0fdc6212cc5e44f8a1f
yellow@wubook.net <yellow@wubook.net>
2009-12-30 16:17:42 +0100

    Module Architecture

---
+++ setup.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/env python
+from distutils.core import setup
+__version__ = '0.1'
+"""
+ WbLibs, from WuBook Labs: http://wubook.net/
+"""
+
+setup(
+    name= 'WbLibs',
+    version= __version__,
+    author= 'Federico Tomassini aka efphe',
+    author_email='yellow@wubook.net',
+    maintainer= 'Federico Tomassini aka efphe',
+    maintainer_email= 'yellow@wubook.net',
+    url= 'http://wubook.net/',
+    packages=['wblibs'],
+    license= "BSD",
+    platforms = ["any"],
+)
+
